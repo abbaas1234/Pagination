b9fe1b4b73cfcfe26490faf12ac04d239e74cbff
abbaas1234 <johndoe@example.com>
2024-01-21 04:28:12 +0330

    initialization to completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,54 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pagination';
+  currentImage = 0;
+
+  checkImageIndex(index: number): boolean {
+    return Math.abs(index - this.currentImage) < 5;
+  }
+
+  images = [
+    {
+      title: 'in the jungle 1',
+      url: 'assets/images/1.jpg'
+    },
+    {
+      title: 'in the jungle 2',
+      url: 'assets/images/2.jpg'
+    },    {
+      title: 'in the jungle 3',
+      url: 'assets/images/3.jpg'
+    },    {
+      title: 'in the jungle 4',
+      url: 'assets/images/4.jpg'
+    },
+    {
+      title: 'in the jungle 1',
+      url: 'assets/images/1.jpg'
+    },
+    {
+      title: 'in the jungle 2',
+      url: 'assets/images/2.jpg'
+    },    {
+      title: 'in the jungle 3',
+      url: 'assets/images/3.jpg'
+    },    {
+      title: 'in the jungle 4',
+      url: 'assets/images/4.jpg'
+    },
+    {
+      title: 'in the jungle 1',
+      url: 'assets/images/1.jpg'
+    },
+    {
+      title: 'in the jungle 2',
+      url: 'assets/images/2.jpg'
+    },    {
+      title: 'in the jungle 3',
+      url: 'assets/images/3.jpg'
+    },    {
+      title: 'in the jungle 4',
+      url: 'assets/images/4.jpg'
+    },
+  ]
 }
